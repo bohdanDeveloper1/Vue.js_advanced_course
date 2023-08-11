1d116293b4202f5add1c3143e10188de13eb2518
Bohdan Boboshko <bogdanbobosko0@gmail.com>
2023-08-11 13:39:21 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container>
+      <b-alert v-shadow show>Default Alert</b-alert>
+    </b-container>
+
+    <b-container class="alert-container">
+      <b-alert v-shadow.fiolet show>Default Alert</b-alert>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from "bootstrap-vue";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
+Vue.directive('shadow', {
+  bind(el, binding){
+    el.style.boxShadow = '21px 18px 10px -9px rgba(199,177,199,1)';
+
+    if(binding.modifiers['fiolet']){
+      el.style.boxShadow = '22px -20px 17px -9px rgba(219,20,219,1)';
+    }
+  }
+});
 
 new Vue({
   render: h => h(App),
